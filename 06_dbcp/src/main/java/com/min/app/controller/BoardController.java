2b80faf845a3fd41d1230907eaeb86fcbf0e4209
bigshotkjh <bigshotkjh@gmail.com>
2024-06-21 20:17:15 +0900

    20240621

--- 06_dbcp/src/main/java/com/min/app/controller/BoardController.java
+++ 06_dbcp/src/main/java/com/min/app/controller/BoardController.java
@@ -1,58 +1,39 @@
-package controller;
+package com.min.app.controller;
 
 import java.io.IOException;
 
-import common.ActionForward;
+import com.min.app.common.ActionForward;
+import com.min.app.service.BoardService;
+import com.min.app.service.BoardServiceImpl;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import service.BookService;
-import service.BookServiceImpl;
 
-public class BookController extends HttpServlet {
+public class BoardController extends HttpServlet {
   
   private static final long serialVersionUID = 1L;
-
+  
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   
     request.setCharacterEncoding("UTF-8");
     
-    String requestURI = request.getRequestURI();    /* http://localhost:9090/mvc/list.do */
-    String contextPath = request.getContextPath();  /*                      /mvc         */
+    String requestURI = request.getRequestURI();
+    String contextPath = request.getContextPath();
     int beginIndex = requestURI.indexOf(contextPath) + contextPath.length() + 1;
-    String urlMapping = requestURI.substring(beginIndex);   /* list.do */
+    String urlMapping = requestURI.substring(beginIndex);
   
-    BookService bookService = new BookServiceImpl();
+    BoardService boardService = new BoardServiceImpl();
     
     ActionForward actionForward = null;
     
     switch(urlMapping) {
     // 단순 이동 (서비스가 필요하지 않다. forward 로 이동한다.)
-    case "write.do":
-      actionForward = new ActionForward("/book/write.jsp", false);
-      break;
-    case "index.do":
-      actionForward = new ActionForward("/index.jsp", false);
-      break;
+    
     // 비즈니스 로직 처리 (서비스가 필요하다.)
     case "list.do":
-      actionForward = bookService.getBooks(request);
-      break;
-    case "detail.do":
-      actionForward = bookService.getBookByNo(request);
-      break;
-    case "register.do":
-      actionForward = bookService.registerBook(request);
-      break;
-    case "delete.do":
-      actionForward = bookService.removeBook(request);
-      break;
-    case "edit.do":
-      actionForward = bookService.editBook(request);
-      break;
-    case "modify.do":
-      actionForward = bookService.modifyBook(request);
+      actionForward = boardService.getBoardList(request);
       break;
     }
     
@@ -63,7 +44,7 @@ public class BookController extends HttpServlet {
         request.getRequestDispatcher(actionForward.getPath()).forward(request, response);
       }
     }
-    
+  
   }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
